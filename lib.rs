52b4d0c3f17510e2ddc524de5fd9b2a2f8e93a3b
D R <metricstream@gamail.con>
2026-04-01 20:54:32 -0700

    added a variant without mut ref but it does extra memory allocations it can be also merged to avoid code duplication

--- lib.rs
+++ lib.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashSet, env, fmt};
+use std::{cell::RefCell, collections::HashSet, env, fmt};
 
 #[cfg(unix)]
 const OPT_PREFIX: char = '-';
@@ -31,7 +31,7 @@ pub enum OptTyp {
 
 /// Specify possible values of command line options
 ///
-#[derive(PartialEq, Debug)]
+#[derive(PartialEq, Debug, Clone)]
 pub enum OptVal {
     Num(i64),
     FNum(f64),
@@ -143,7 +143,8 @@ impl CLI {
     pub fn oper_description(&mut self, descr: &str) -> Result<&mut Self, OptError> {
         if !self.oper_requested {
             Err(OptError {
-                cause: format!("an operation description can be defined after a set - use_oper()"),
+                cause: "an operation description can be defined after a set - use_oper()"
+                    .to_string(),
             })
         } else {
             self.oper_descr = Some(descr.to_string());
@@ -282,15 +283,246 @@ impl CLI {
                 if !consumed {
                     self.unknown.push(arg)
                 }
+            } else if self.oper.is_none() && self.oper_requested {
+                self.oper = Some(arg.clone())
             } else {
-                if self.oper.is_none() && self.oper_requested {
-                    self.oper = Some(arg.clone())
-                } else {
-                    self.args.push(arg)
-                }
+                self.args.push(arg)
             }
             self.oper_requested = false;
         }
         self.unprocessed = false
+    }
+}
+
+pub struct CliMut {
+    cli: RefCell<CLI>,
+}
+impl Default for CliMut {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl CliMut {
+    /// Create an empty CLI arguments descriptor
+    ///
+    pub fn new() -> Self {
+        CliMut {
+            cli: RefCell::new(CLI {
+                args: vec![],
+                opts: vec![],
+                descr: None,
+                oper: Default::default(),
+                oper_requested: false,
+                oper_descr: Default::default(),
+                unprocessed: true,
+                unknown: vec![],
+            }),
+        }
+    }
+
+    /// Creates a new argument option
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let _ = cli.opt("c", OptTyp::None).inspect_err(|e| eprintln!("{e}"));
+    /// ```
+    pub fn opt(&self, name: &str, t: OptTyp) -> Result<&Self, OptError> {
+        let mut cli = self.cli.borrow_mut();
+        if !cli.unprocessed {
+            return Err(OptError {
+                cause: format!("the option {name} can't be set after parsing arguments"),
+            });
+        }
+        for opt in &cli.opts {
+            if opt.nme == name {
+                return Err(OptError {
+                    cause: format!("repeating option {name}"),
+                });
+            }
+        }
+        cli.opts.push(CliOpt {
+            t,
+            nme: name.to_string(),
+            descr: None,
+            v: None,
+        });
+        Ok(self)
+    }
+    /// Specify common CLI description
+    ///
+    pub fn description(&self, descr: &str) -> &Self {
+        let mut cli = self.cli.borrow_mut();
+        match cli.opts.last_mut() {
+            Some(element) => element.descr = Some(descr.to_string()),
+            _ => cli.descr = Some(descr.to_string()),
+        }
+        self
+    }
+    /// Use an operation as the first arguments
+    ///
+    pub fn use_oper(&self) -> &Self {
+        let mut cli = self.cli.borrow_mut();
+        cli.oper_requested = true;
+        self
+    }
+    /// Specify an operation description
+    ///
+    pub fn oper_description(&self, descr: &str) -> Result<&Self, OptError> {
+        let mut cli = self.cli.borrow_mut();
+        if !cli.oper_requested {
+            Err(OptError {
+                cause: "an operation description can be defined after a set - use_oper()"
+                    .to_string(),
+            })
+        } else {
+            cli.oper_descr = Some(descr.to_string());
+            Ok(self)
+        }
+    }
+    /// Get the CLI description
+    ///
+    pub fn get_description(&self) -> Option<String> {
+        let cli = self.cli.borrow();
+        let mut descr = String::new();
+        if let Some(some_descr) = &cli.descr {
+            descr += some_descr
+        }
+        if let Some(some_descr) = &cli.oper_descr {
+            descr += &format!("\n{some_descr}")
+        }
+        for opt in &cli.opts {
+            descr += &format!("\n{OPT_PREFIX}{}", opt.nme);
+            if let Some(some_descr) = &opt.descr {
+                descr += &format!("\t{some_descr}")
+            }
+        }
+        if descr.is_empty() { None } else { Some(descr) }
+    }
+    /// Get a CLI option
+    ///
+    pub fn get_opt(&self, name: &str) -> Option<OptVal> {
+        if self.cli.borrow().unprocessed {
+            self.parse()
+        }
+        let opts = &self.cli.borrow().opts;
+        for opt in opts {
+            if opt.nme == name {
+                return opt.v.clone();
+            }
+        }
+        None
+    }
+    /// Returns first argument as an operation
+    ///
+    /// some CLI tools, as git consider first argument as operation/command
+    ///
+    /// the argument will be also added in arguments vec itself
+    pub fn get_oper(&self) -> Option<String> {
+        if self.cli.borrow().unprocessed {
+            self.parse()
+        }
+        self.cli.borrow().oper.clone()
+    }
+    /// Get CLI arguments
+    ///
+    pub fn args(&self) -> Vec<String> {
+        if self.cli.borrow().unprocessed {
+            self.parse()
+        }
+        self.cli.borrow().args.clone()
+    }
+
+    /// Get errors
+    ///
+    /// Returns a vector of unrecognized options or None
+    pub fn get_errors(&self) -> Option<Vec<String>> {
+        if self.cli.borrow().unprocessed {
+            self.parse()
+        }
+        if self.cli.borrow().unknown.is_empty() {
+            None
+        } else {
+            Some(self.cli.borrow().unknown.clone())
+        }
+    }
+
+    fn parse(&self) {
+        let mut cli = self.cli.borrow_mut();
+        let mut args = env::args();
+        args.next(); // swallow first
+        while let Some(arg) = args.next() {
+            if let Some(sarg) = arg.strip_prefix(OPT_PREFIX) {
+                // TODO eat extra -'s
+                let mut consumed = false;
+                for opt in &mut cli.opts {
+                    if opt.nme == sarg {
+                        match opt.t {
+                            OptTyp::Num => {
+                                if let Some(val) = args.next() {
+                                    match val.parse::<i64>() {
+                                        Ok(num) => opt.v = Some(OptVal::Num(num)),
+                                        _ => opt.v = Some(OptVal::Unmatch),
+                                    }
+                                }
+                            }
+                            OptTyp::None => opt.v = Some(OptVal::Empty),
+                            OptTyp::FNum => {
+                                if let Some(val) = args.next() {
+                                    match val.parse::<f64>() {
+                                        Ok(num) => opt.v = Some(OptVal::FNum(num)),
+                                        _ => opt.v = Some(OptVal::Unmatch),
+                                    }
+                                }
+                            }
+                            OptTyp::Str => {
+                                if let Some(str) = args.next() {
+                                    opt.v = Some(OptVal::Str(str))
+                                }
+                            }
+                            OptTyp::InStr => (),
+                        }
+                        consumed = true;
+                    } else if opt.t == OptTyp::InStr && sarg.starts_with(&opt.nme) {
+                        if opt.v.is_none() {
+                            opt.v = Some(OptVal::Arr(HashSet::new()))
+                        }
+                        match &mut opt.v {
+                            &mut Some(OptVal::Arr(ref mut set)) => {
+                                if let Some(pair) =
+                                    sarg.strip_prefix(&opt.nme).unwrap().split_once('=')
+                                {
+                                    set.insert((pair.0.to_string(), pair.1.to_string()));
+                                } else {
+                                    set.insert((
+                                        sarg.strip_prefix(&opt.nme).unwrap().to_string(),
+                                        String::new(),
+                                    ));
+                                }
+                            }
+                            _ => {
+                                // somehow to report data inconsistency
+                                opt.v = Some(OptVal::Arr(HashSet::new()))
+                            }
+                        }
+                        consumed = true;
+                    } else if opt.nme.len() == 1 && sarg.contains(&opt.nme) && opt.t == OptTyp::None
+                    {
+                        opt.v = Some(OptVal::Empty);
+                        consumed = true;
+                    }
+                }
+                if !consumed {
+                    cli.unknown.push(arg)
+                }
+            } else if cli.oper.is_none() && cli.oper_requested {
+                cli.oper = Some(arg.clone())
+            } else {
+                cli.args.push(arg)
+            }
+            cli.oper_requested = false;
+        }
+        cli.unprocessed = false
     }
 }
